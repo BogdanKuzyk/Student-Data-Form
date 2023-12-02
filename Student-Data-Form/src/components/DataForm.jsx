f47991e7fff76673f37c7a0191dce7d54061087d
Bogdan Kuzyk <bogdankuzyk14@gmail.com>
2023-12-02 15:50:08 +0200

    Create State, add event listeners on form and inputs and create handler fumctions

--- Student-Data-Form/src/components/DataForm.jsx
+++ Student-Data-Form/src/components/DataForm.jsx
@@ -1,21 +1,53 @@
 import "./DataForm.css";
+import { useState } from "react";
 
 const DataForm = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const firstNameHandler = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const lastNameHandler = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const emailHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    const studentData = {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+    };
+
+    console.log(studentData);
+  };
+
   return (
-    <form>
-      <div className="new-cost__controls">
-        <div className="new-cost__control">
+    <form onSubmit={formSubmitHandler}>
+      <div>
+        <div>
           <label>First Name</label>
-          <input type="text"></input>
+          <input type="text" onChange={firstNameHandler}></input>
         </div>
-        <div className="new-cost__control">
+        <div>
           <label>Last Name</label>
-          <input type="text"></input>
+          <input type="text" onChange={lastNameHandler}></input>
         </div>
-        <div className="new-cost__control">
+        <div>
           <label>Email</label>
-          <input type="text"></input>
+          <input type="text" onChange={emailHandler}></input>
         </div>
+      </div>
+      <div>
+        <button type="submit">Submit</button>
       </div>
     </form>
   );
